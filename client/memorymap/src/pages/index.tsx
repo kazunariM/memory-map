b9dc14a3d5f2801b28198c339207e17369ae6634
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2023-03-22 21:17:40 +0900

    first commit

---
+++ client/memorymap/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>思い出マップ</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
