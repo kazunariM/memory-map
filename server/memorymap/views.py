619f5fb962cc9be559805f459f6c785ada080f93
= <=>
2023-03-25 19:11:05 +0900

    b

--- server/memorymap/views.py
+++ server/memorymap/views.py
@@ -5,9 +5,10 @@ from .models import Event
 from .serializers import EventSerializer
 #from rest_framework import generics
 
-
+#aa
 class EventListAPIView(ListAPIView):#10件取得
-    queryset = Event.objects.all()
+    #queryset = Event.objects.all()
+    queryset = Event.objects.all().order_by('-created_at')[:10]
     print(queryset[0].title)
     print(queryset[0].uuid)
     print(queryset[0].text)
