91022a50751b2be30ff83e769741a3eae5c01c2f
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2023-03-25 13:54:49 +0900

    feat: トップページ追加

---
+++ client/memorymap/src/map/display.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, FC, useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent, useMapEvents } from 'react-leaflet'
+import * as L from 'leaflet';
+import { LatLng } from 'leaflet';
+import "leaflet/dist/leaflet.css"
+
+const Map = () => {
+    const defaultPosition = new LatLng(35.68209024197379, 139.76712479998636)
+
+    return (
+        <>
+        <MapContainer
+            center={defaultPosition}
+            zoom={13}
+            scrollWheelZoom={true}
+            style={{
+                width: '100%',
+                height: '70vh'
+            }}
+        >
+            <TileLayer
+                url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors' 
+            />
+
+        </MapContainer>
+        </>
+    )
+}
+
+export default Map
--- client/memorymap/src/pages/index.tsx
+++ client/memorymap/src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import { Inter } from 'next/font/google'
+
+const Map = dynamic(() => import('@/map/display'), { ssr: false })
+
+import styles from '@/styles/index.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +14,33 @@ export default function Home() {
       <Head>
         <title>思い出マップ</title>
       </Head>
-      <main>
+      <header>
+        <nav>
+          <h1>思い出マップ</h1>
+        </nav>
+      </header>
+      <main className={styles.main}>
+
+        <div className={styles.side}>
+          <aside>
+            <ul>
+              <li>ホーム</li>
+              <li>投稿</li>
+            </ul>
+          </aside>
+        </div>
+
+        <div className={styles.contents}>
+          <div className={styles.map_area}>
+            <form><label>検索</label><input type="text" /></form>
+            <Map />
+          </div>
+          <div className={styles.list_area}>
+            <div>
+              <p>event</p>
+            </div>
+          </div>
+        </div>
 
       </main>
     </>
