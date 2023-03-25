352cdfdda191a21bb08c970c0cb2a9f0ea590b44
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2023-03-25 12:39:53 +0900

    fix: auto_nowやManyToManyFieldの修正
    UUIDを使うように変更

--- server/memorymap/models.py
+++ server/memorymap/models.py
@@ -8,32 +8,34 @@ import uuid, os
 
 class User(AbstractUser):
     def img_path(self, filename):
-        result = 'stampbg/{}{}'.format(str(uuid.uuid4().hex), os.path.splitext(filename)[-1])
+        result = 'profile/{}{}'.format(str(uuid.uuid4().hex), os.path.splitext(filename)[-1])
         return result
     
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, primary_key=True)
     prof_image = models.ImageField(upload_to=img_path ,null=True, blank=True)
     
-class Event(AbstractUser):
+class Event(models.Model):
     """みずがめ
 
     Args:
         thumbnail (ImageField): 作成したが、間違っている可能性あり
     """
     def img_path(self, filename):#Userと同じ
-        result = 'stampbg/{}{}'.format(str(uuid.uuid4().hex), os.path.splitext(filename)[-1])
+        result = 'event/{}{}'.format(str(uuid.uuid4().hex), os.path.splitext(filename)[-1])
         return result
     
 
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, primary_key=True)
     title = models.CharField(max_length = 127)
     #text = models.CharField(_(""), max_length=50)
     text = models.CharField(max_length=511,blank=True)
     thumbnail = models.ImageField( upload_to=img_path, null = True,blank = True)
-    create_at = models.DateTimeField( auto_now_add=False,auto_now_add=True)#insertされた時だけ日付変わる
+    create_at = models.DateTimeField( auto_now=False,auto_now_add=True)#insertされた時だけ日付変わる
     hold_date = models.DateTimeField( auto_now=False, auto_now_add=False)#insertされた時だけ日付変わる
     edited_at = models.DateTimeField( auto_now=True, auto_now_add=False)#save()などで更新されたときに日付変わる
 
     lat = models.FloatField((""))#FloatFieldは2進数で小数を表す
     lon = models.FloatField((""))#FloatFieldは2進数で小数を表す
-    member = User(blank = True,related_name = 'events')
+    member = models.ManyToManyField(User, blank = True,related_name = 'events')
 
     
